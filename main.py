d6dbc63399f9881cbd2cc6afa7a6ca2c36e5c27f
Lain <vvz.dev@gmail.com>
2022-02-02 23:11:30 +0300

    AST generator implemented

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import string as _string
 from enum import Enum, auto
 
 # <CONSTANTS>
+from typing import Callable
+
 LETTERS = _string.ascii_letters
 DIGITS = _string.digits
 
 FN_OPERATOR = '=>'
+EQ_OPERATOR = '='
 PLUS_OPERATOR = '+'
 MINUS_OPERATOR = '-'
 MUL_OPERATOR = '*'
@@ -138,11 +141,11 @@ class Lexer:
         return identifier
 # </LEXER>
 
+
 # <NODES>
 class Nodes:
-
     class Node:
-        def __init__(self, token: Token):
+        def __init__(self, token: Token = None):
             self.token = token
 
         def __str__(self):
@@ -164,40 +167,53 @@ class Nodes:
             return self.name
 
     class BinOp(Node):
-        def __init__(self, left: Nodes.Node, op: Token, right: Nodes.Node):
-            super().__init__(op)
-            self.left = left
-            self.right = right
+        def __init__(self, left_operand: Nodes.Node, operator: Token, right_operand: Nodes.Node):
+            super().__init__()
+            self.left_operand = left_operand
+            self.operator = operator
+            self.right_operand = right_operand
 
         def __str__(self):
-            return f'({self.left}, {self.token}, {self.right})'
+            return f'({self.left_operand}, {self.operator}, {self.right_operand})'
+
+    class UnaryOp(Node):
+        def __init__(self, operator: Token, operand: Nodes.Node):
+            super().__init__()
+            self.operator = operator
+            self.operand = operand
+
+        def __str__(self):
+            return f'({self.operator}, {self.operand})'
 
     class VariableAssignment(Node):
         def __init__(self, variable: Token, expression: Nodes.Node):
-            super().__init__(variable)
+            super().__init__()
+            self.variable = variable
             self.expression = expression
 
         def __str__(self):
-            return f'({self.token} = {self.expression})'
+            return f'({self.variable} = {self.expression})'
 
     class FunctionAssignment(Node):
         def __init__(
                 self,
-                function: Token,
+                function: Nodes.Identifier,
                 variables: list[Nodes.Identifier],
                 expression: Nodes.Node
         ):
-            super().__init__(function)
+            super().__init__()
+            self.function = function
             self.variables = variables
             self.expression = expression
 
         def __str__(self):
             variables_names = ' '.join(map(lambda v: v.name, self.variables))
-            return f'({self.token.value} [{variables_names}] = {self.expression})'
+            return f'({self.function.name} [{variables_names}] = {self.expression})'
 
     class FunctionCall(Node):
         def __init__(self, function: Token, variables: list[Nodes.Identifier]):
-            super().__init__(function)
+            super().__init__()
+            self.function = function
             self.variables = variables
 
         def __str__(self):
@@ -206,7 +222,110 @@ class Nodes:
 # </NODES>
 
 
-a = Lexer("fn doubleIt x => x * 2")
+# <Parser>
+class Parser:
+    tokens: list[Token]
+    token: Token | None
+    pos: int
+
+    def __init__(self, tokens: list[Token]):
+        self.tokens = tokens
+        self.pos = -1
+        self.token = None
+        self.advance()
+
+    def advance(self):
+        self.pos += 1
+        if self.pos < len(self.tokens):
+            self.token = self.tokens[self.pos]
+        else:
+            self.token = None
+
+    def generateAST(self) -> Nodes.Node:
+        ast = self.expression()
+        if self.token.type is not TokenType.EOF:
+            raise Exception('Invalid Syntax')
+        return ast
+
+    def factor(self) -> Nodes.Node | Nodes.Identifier:
+        token = self.token
+
+        match token.type:
+            case TokenType.OPERATOR:
+                # Unary Operator
+                if token.value in (PLUS_OPERATOR, MINUS_OPERATOR):
+                    self.advance()
+                    factor = self.factor()
+                    return Nodes.UnaryOp(token, factor)
+
+                # Variable assign
+                elif token.value == EQ_OPERATOR:
+                    self.advance()
+                    expression = self.expression()
+                    return Nodes.VariableAssignment(token, expression)
+                elif token.value == FN_OPERATOR:
+                    self.advance()
+                    return Nodes.Node()
+
+                raise Exception('Invalid Operator')
+            case TokenType.NUMBER:
+                self.advance()
+                return Nodes.Number(token)
+            case TokenType.LEFT_PAR:
+                self.advance()
+                expression = self.expression()
+                if self.token.type is TokenType.RIGHT_PAR:
+                    self.advance()
+                    return expression
+            case TokenType.IDENTIFIER:
+                self.advance()
+                return Nodes.Identifier(token)
+            case TokenType.KEYWORD:
+                self.advance()
+                # Function assignment
+                if token.value == FN_KEYWORD:
+                    function_identifier = self.factor()
+                    variables = []
+                    last_return = self.factor()
+
+                    # Collect variables until '=>' operator is met
+                    while isinstance(last_return, Nodes.Identifier):
+                        variables.append(last_return)
+                        last_return = self.factor()
+
+                    expression = self.expression()
+                    return Nodes.FunctionAssignment(function_identifier, variables, expression)
+
+                raise Exception('Unknown Keyword')
+
+        raise Exception('Unknown Token Type')
+
+    def term(self) -> Nodes.Node:
+        term_operators = (MUL_OPERATOR, DIV_OPERATOR, REM_OPERATOR)
+        return self.bin_op(self.factor, term_operators)
+
+    def expression(self) -> Nodes.Node:
+        expression_operators = (PLUS_OPERATOR, MINUS_OPERATOR)
+        return self.bin_op(self.term, expression_operators)
+
+    def bin_op(
+            self,
+            fn: Callable[..., Nodes.Node],
+            operators: tuple[str, ...]
+    ) -> Nodes.BinOp:
+        left_operand = fn()
+
+        while self.token.value in operators:
+            operator = self.token
+            self.advance()
+            right_operand = fn()
+            left_operand = Nodes.BinOp(left_operand, operator, right_operand)
+
+        return left_operand
+# </Parser>
+
+a = Lexer("(1 + 2) * 3")
 tokens = a.tokenize()
-b = Nodes.FunctionAssignment(tokens[1], [Nodes.Identifier(tokens[2])], Nodes.BinOp(Nodes.Number(tokens[4]), tokens[5], Nodes.Identifier(tokens[6])))
-print(b)
+
+b = Parser(tokens)
+print(b.generateAST())
