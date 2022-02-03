ca9f85608f2862aa678c9f25624f4d0bbfe9ad2b
Lain <vvz.dev@gmail.com>
2022-02-03 12:03:37 +0300

    basic function support added

--- main.py
+++ main.py
@@ -147,6 +147,11 @@ class Nodes:
     class Node:
         def __init__(self, token: Token = None):
             self.token = token
+            self.ids: set[Nodes.Identifier] = set()
+
+        def add_identifier(self, identifier: Nodes.Node):
+            if isinstance(identifier, Nodes.Identifier):
+                self.ids.add(identifier)
 
         def __str__(self):
             return str(self.token)
@@ -163,6 +168,8 @@ class Nodes:
             super().__init__(token)
             self.name = self.token.value
 
+            self.add_identifier(self)
+
         def __str__(self):
             return self.name
 
@@ -173,6 +180,14 @@ class Nodes:
             self.operator = operator
             self.right_operand = right_operand
 
+            for identifier in left_operand.ids:
+                self.add_identifier(identifier)
+            self.add_identifier(left_operand)
+
+            for identifier in right_operand.ids:
+                self.add_identifier(identifier)
+            self.add_identifier(right_operand)
+
         def __str__(self):
             return f'({self.left_operand}, {self.operator}, {self.right_operand})'
 
@@ -182,6 +197,10 @@ class Nodes:
             self.operator = operator
             self.operand = operand
 
+            for identifier in operand.ids:
+                self.add_identifier(identifier)
+            self.add_identifier(operand)
+
         def __str__(self):
             return f'({self.operator}, {self.operand})'
 
@@ -190,6 +209,8 @@ class Nodes:
             super().__init__()
             self.variable = variable
             self.expression = expression
+
+            self.add_identifier(expression)
 
         def __str__(self):
             return f'({self.variable} = {self.expression})'
@@ -206,6 +227,20 @@ class Nodes:
             self.arguments = arguments
             self.expression = expression
 
+            self.add_identifier(function)
+            self.add_identifier(expression)
+            for arg in arguments:
+                self.add_identifier(arg)
+            self.__check_for_args_scope()
+
+        # That's just bad...
+        def __check_for_args_scope(self):
+            if len(set(self.arguments)) != len(self.arguments):
+                raise Exception('Repeating arguments in function definition')
+            if len(self.expression.ids) != len(self.arguments):
+                print(self.expression.ids, self.arguments)
+                raise Exception('Unknown identifier in function body')
+
         def __str__(self):
             variables_names = ' '.join(map(lambda v: v.name, self.arguments))
             return f'({FN_KEYWORD} {self.function.name} [{variables_names}] => {self.expression})'
@@ -215,6 +250,11 @@ class Nodes:
             super().__init__()
             self.function = function
             self.arguments = arguments
+
+            self.add_identifier(function)
+            # Not sure about it?
+            for arg in arguments:
+                self.add_identifier(arg)
 
         def __str__(self):
             # arguments = ' '.join(map(lambda v: v.name, self.arguments))
@@ -315,6 +355,12 @@ class Parser:
     ) -> Nodes.BinOp | Nodes.Node:
         left_operand = fn()
 
+        # This is awful solution lol but whatever (it should be done in the factor)
+        if self.token.value == EQ_OPERATOR:
+            self.advance()
+            expression = self.expression()
+            return Nodes.VariableAssignment(left_operand.token, expression)
+
         # Check if it is function call
         # Example: add echo 4 echo 3 -> 7.
         arguments = []
@@ -334,8 +380,191 @@ class Parser:
 # </Parser>
 
 
-a = Lexer("fn addFunction f g value => f value + g value")
-tokens = a.tokenize()
+# <Objects>
+class Objects:
 
-b = Parser(tokens)
-print(b.generateAST())
+    class Object:
+        def __init__(self):
+            pass
+
+        def __repr__(self):
+            return self.__str__()
+
+    class Number(Object):
+        def __init__(self, value: int):
+            super().__init__()
+            self.value = value
+
+        def __str__(self):
+            return str(self.value)
+
+        def __add__(self, other: Objects.Number):
+            return Objects.Number(self.value + other.value)
+
+        def __sub__(self, other: Objects.Number):
+            return Objects.Number(self.value - other.value)
+
+        def __mul__(self, other: Objects.Number):
+            return Objects.Number(self.value * other.value)
+
+        def __floordiv__(self, other: Objects.Number):
+            return Objects.Number(self.value // other.value)
+
+        def __mod__(self, other: Objects.Number):
+            return Objects.Number(self.value % other.value)
+
+        def __pow__(self, other: Objects.Number, modulo=None):
+            return Objects.Number(self.value ** other.value)
+
+    class Variable(Object):
+        def __init__(self, name: str, value: Objects.Object):
+            super().__init__()
+            self.name = name
+            self.value = value
+
+        def __str__(self):
+            return self.name
+
+    class Function(Object):
+        def __init__(self, name: str, variables: list[Nodes.Identifier], body: Nodes.Node):
+            super().__init__()
+            self.name = name
+            self.variables = variables
+            self.body = body
+
+        def call(self, arguments: list[Nodes.Identifier]) -> Objects.Object:
+            binded_args = {var.name: arguments[i] for i, var in enumerate(self.variables)}
+            scope = Interpreter(variables=binded_args)
+            return scope.input_ast(self.body)
+
+        def __str__(self):
+            return f'{self.name} [{" ".join(map(str, self.variables))}] => {self.body}'
+# </Object>
+
+
+# <Interpreter>
+class Interpreter:
+    ast: Nodes.Node
+    variables: dict[str, Objects.Variable]
+    functions: dict[str, Objects.Function]
+
+    def __init__(self, variables=None, functions=None):
+        self.variables = variables or dict()
+        self.functions = functions or dict()
+
+    def input(self, code: str) -> Objects.Object:
+        tokens = Lexer(code).tokenize()
+        ast = Parser(tokens).generateAST()
+        return self.visit(ast)
+
+    # TODO: remove later
+    def input_ast(self, ast: Nodes.Node) -> Objects.Object:
+        return self.visit(ast)
+
+    def visit(self, node: Nodes.Node) -> Objects.Object:
+        if isinstance(node, Nodes.Number):
+            return self.visit_number_node(node)
+        if isinstance(node, Nodes.Identifier):
+            return self.visit_identifier_node(node)
+        if isinstance(node, Nodes.BinOp):
+            return self.visit_binary_op_node(node)
+        if isinstance(node, Nodes.UnaryOp):
+            return self.visit_unary_op_node(node)
+        if isinstance(node, Nodes.FunctionCall):
+            return self.visit_function_call_node(node)
+        if isinstance(node, Nodes.VariableAssignment):
+            return self.visit_var_assign_node(node)
+        if isinstance(node, Nodes.FunctionAssignment):
+            return self.visit_func_assign_node(node)
+
+        raise Exception('Unknown Node')
+
+
+    def visit_number_node(self, node: Nodes.Number) -> Objects.Number:
+        return Objects.Number(int(node.token.value))
+
+    def visit_identifier_node(self, node: Nodes.Identifier) -> Objects.Object:
+        name = node.name
+        if name in self.variables:
+            return self.variables[name]
+
+        if name in self.functions:
+            return self.functions[name]
+        raise Exception('Unknown identifier')
+
+    def visit_var_assign_node(self, node: Nodes.VariableAssignment) -> Objects.Object:
+        variable_name = node.variable.value
+        if variable_name in self.functions:
+            raise Exception('Cannot reassign function to variable (Name conflict)')
+        variable_value = self.visit(node.expression)
+        variable = Objects.Variable(variable_name, variable_value)
+        self.variables[variable.name] = variable
+        return variable.value
+
+    def visit_func_assign_node(self, node: Nodes.FunctionAssignment) -> Objects.Function:
+        function_name = node.function.name
+        if function_name in self.variables:
+            raise Exception('Cannot reassign variable to function (Name conflict)')
+        function_vars = node.arguments
+        function_body = node.expression
+
+        function = Objects.Function(function_name, function_vars, function_body)
+        self.functions[function.name] = function
+        return function
+
+    def visit_function_call_node(self, node: Nodes.FunctionCall) -> Objects.Object:
+        function = self.functions[node.function.name]
+        arguments = node.arguments
+
+        # TODO: implement function call
+        return function.call(arguments)
+
+    def visit_unary_op_node(self, node: Nodes.UnaryOp) -> Objects.Number:
+        num = self.visit(node.operand)
+        if not isinstance(num, Objects.Number):
+            raise Exception('Unary operation can be applied only to numbers')
+
+        if node.operator.type is MINUS_OPERATOR:
+            num *= Objects.Number(-1)
+
+        return num
+
+    def visit_binary_op_node(self, node: Nodes.BinOp) -> Objects.Number:
+        left = self.visit(node.left_operand)
+        right = self.visit(node.right_operand)
+
+        print(type(left), type(right))
+
+        if isinstance(left, Nodes.Number):
+            left = Objects.Number(int(left.token.value))
+        if isinstance(right, Nodes.Number):
+            right = Objects.Number(int(right.token.value))
+
+        if not isinstance(left, Objects.Number) or not isinstance(right, Objects.Number):
+            raise Exception('Binary operation can be applied only to numbers')
+
+        op = node.operator.value
+        if op == PLUS_OPERATOR:
+            return left + right
+        if op == MINUS_OPERATOR:
+            return left - right
+        if op == MUL_OPERATOR:
+            return left * right
+        if op == DIV_OPERATOR:
+            if right.value == 0:
+                raise Exception('Division by zero')
+            return left // right
+        if op == REM_OPERATOR:
+            if right.value == 0:
+                raise Exception('Division by zero')
+            return left % right
+
+        raise Exception('Unknown operator')
+# </Interpreter>
+
+interpreter = Interpreter()
+interpreter.input("fn echo x => x")
+interpreter.input("fn add x y => x + y")
+var = interpreter.input("add echo 4 echo 3")
+print(var)
+
